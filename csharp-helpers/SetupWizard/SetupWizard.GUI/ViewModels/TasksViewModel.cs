8cfc89a56fafebc5da7a39422005df0f63ee8c9f
ArchLeaders <archparliament@gmail.com>
2022-04-26 13:55:30 -0700

    DataGrid random data; TaskViewModel class

---
+++ csharp-helpers/SetupWizard/SetupWizard.GUI/ViewModels/TasksViewModel.cs
@@ -0,0 +1,89 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetupWizard.GUI.ViewModels
+{
+    public class TasksViewModel : Screen
+    {
+        private bool _synced;
+        public bool Synced
+        {
+            get => _synced;
+            set => SetAndNotify(ref _synced, value);
+        }
+
+        private TimeOnly _time;
+        public TimeOnly Time
+        {
+            get => _time;
+            set => SetAndNotify(ref _time, value);
+        }
+
+        private BindableCollection<string> _days;
+        public BindableCollection<string> Days
+        {
+            get => _days;
+            set => SetAndNotify(ref _days, value);
+        }
+
+        private uint _channel;
+        public uint Channel
+        {
+            get => _channel;
+            set => SetAndNotify(ref _channel, value);
+        }
+
+        private uint _role;
+        public uint Role
+        {
+            get => _role;
+            set => SetAndNotify(ref _role, value);
+        }
+
+        private uint _user;
+        public uint User
+        {
+            get => _user;
+            set => SetAndNotify(ref _user, value);
+        }
+
+        private BindableCollection<string> _sequence;
+        public BindableCollection<string> Sequence
+        {
+            get => _sequence;
+            set => SetAndNotify(ref _sequence, value);
+        }
+
+        private string _message;
+        public string Message
+        {
+            get => _message;
+            set => SetAndNotify(ref _message, value);
+        }
+
+        private BindableCollection<uint> _channels;
+        public BindableCollection<uint> Channels
+        {
+            get => _channels;
+            set => SetAndNotify(ref _channels, value);
+        }
+
+        private BindableCollection<uint> _roles;
+        public BindableCollection<uint> Roles
+        {
+            get => _roles;
+            set => SetAndNotify(ref _roles, value);
+        }
+
+        private BindableCollection<uint> _users;
+        public BindableCollection<uint> Users
+        {
+            get => _users;
+            set => SetAndNotify(ref _users, value);
+        }
+    }
+}
---
+++ csharp-helpers/SetupWizard/SetupWizard.GUI/ViewResources/Helpers/RanDataAccess.cs
@@ -0,0 +1,56 @@
+﻿using SetupWizard.GUI.ViewModels;
+using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetupWizard.GUI.ViewResources.Helpers
+{
+    internal class RanDataAccess
+    {
+        public static BindableCollection<TasksViewModel> Get()
+        {
+            BindableCollection<TasksViewModel> tasks = new();
+
+            tasks.Add(new()
+            {
+                Synced = true,
+                Time = TimeOnly.Parse("1:20PM"),
+                Days = new() { "sun", "mon" },
+                Channel = 0,
+                Role = 0,
+                User = 0,
+                Sequence = new() { "user_a", "user_w" },
+                Message = "@role $user needs to do somethig at $time_now"
+            });
+
+            tasks.Add(new()
+            {
+                Synced = false,
+                Time = TimeOnly.Parse("2:50PM"),
+                Days = new() { "tue", "thu" },
+                Channel = 6,
+                Role = 2,
+                User = 5,
+                Sequence = new() { "user_w" },
+                Message = "@role $user needs to do somethig at $time_now"
+            });
+
+            tasks.Add(new()
+            {
+                Synced = true,
+                Time = TimeOnly.Parse("3:25PM"),
+                Days = new() { "sat", "mon" },
+                Channel = 7,
+                Role = 1,
+                User = 3,
+                Sequence = new() { "user_w", "user_a" },
+                Message = "@role $user needs to do somethig at $time_now"
+            });
+
+            return tasks;
+        }
+    }
+}
