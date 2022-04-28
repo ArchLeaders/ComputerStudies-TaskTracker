2e3adf152047770314d7128c2e048de6c4ddfff0
ArchLeaders <archparliament@gmail.com>
2022-04-28 13:15:27 -0700

    Add uint dataId and make data string

--- csharp-helpers/SetupWizard/SetupWizard.GUI/ViewModels/TasksViewModel.cs
+++ csharp-helpers/SetupWizard/SetupWizard.GUI/ViewModels/TasksViewModel.cs
@@ -23,63 +23,84 @@ namespace SetupWizard.GUI.ViewModels
             set => SetAndNotify(ref _time, value);
         }
 
-        private BindableCollection<string> _days;
+        private BindableCollection<string> _days = new();
         public BindableCollection<string> Days
         {
             get => _days;
             set => SetAndNotify(ref _days, value);
         }
 
-        private uint _channel;
-        public uint Channel
+        private string _channel = "";
+        public string Channel
         {
             get => _channel;
             set => SetAndNotify(ref _channel, value);
         }
 
-        private uint _role;
-        public uint Role
+        private string _role = "";
+        public string Role
         {
             get => _role;
             set => SetAndNotify(ref _role, value);
         }
 
-        private uint _user;
-        public uint User
+        private string _user = "";
+        public string User
         {
             get => _user;
             set => SetAndNotify(ref _user, value);
         }
 
-        private BindableCollection<string> _sequence;
+        private uint _channelId = 0;
+        public uint ChannelId
+        {
+            get => _channelId;
+            set => SetAndNotify(ref _channelId, value);
+        }
+
+        private uint _roleId = 0;
+        public uint RoleId
+        {
+            get => _roleId;
+            set => SetAndNotify(ref _roleId, value);
+        }
+
+        private uint _userId = 0;
+        public uint UserId
+        {
+            get => _userId;
+            set => SetAndNotify(ref _userId, value);
+        }
+
+        private BindableCollection<string> _sequence = new();
         public BindableCollection<string> Sequence
         {
             get => _sequence;
             set => SetAndNotify(ref _sequence, value);
         }
 
-        private string _message;
+        private string _message = "";
         public string Message
         {
             get => _message;
             set => SetAndNotify(ref _message, value);
         }
 
-        private BindableCollection<uint> _channels;
+        private BindableCollection<uint> _channels = new();
         public BindableCollection<uint> Channels
         {
             get => _channels;
             set => SetAndNotify(ref _channels, value);
         }
 
-        private BindableCollection<uint> _roles;
+        private BindableCollection<uint> _roles = new();
         public BindableCollection<uint> Roles
         {
             get => _roles;
             set => SetAndNotify(ref _roles, value);
         }
 
-        private BindableCollection<uint> _users;
+        private BindableCollection<uint> _users = new();
         public BindableCollection<uint> Users
         {
             get => _users;
