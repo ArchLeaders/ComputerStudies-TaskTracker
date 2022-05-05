815c652b7442ae1f75ccea3ff5bb99fa6a553b32
ArchLeaders <archparliament@gmail.com>
2022-05-04 17:36:01 -0700

    Var Editor View/ViewModel

---
+++ csharp-helpers/SetupWizard/SetupWizard.GUI/ViewModels/VarViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetupWizard.GUI.ViewModels
+{
+    public class VarViewModel : Screen
+    {
+        private string _key = "placeholder_key";
+        public string Key
+        {
+            get => _key;
+            set => SetAndNotify(ref _key, value);
+        }
+
+        private string _value = "placeholder_value";
+        public string Value
+        {
+            get => _value;
+            set => SetAndNotify(ref _value, value);
+        }
+
+        public VarViewModel() { }
+        public VarViewModel(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
