9383b7c7f94b1ad283e767356f463bc3760099a0
ArchLeaders <archparliament@gmail.com>
2022-05-10 17:20:03 -0700

    Optimize Server class for data transfer rather than data holding

---
+++ csharp-helpers/SetupWizard/SetupWizard.GUI/Models/Server.cs
@@ -0,0 +1,46 @@
+﻿using SetupWizard.GUI.ViewModels;
+using Stylet;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace SetupWizard.GUI.Models
+{
+    public class Server
+    {
+        /// <summary>
+        /// Bindable dictionary of Channels
+        /// </summary>
+        [JsonIgnore()]
+        public BindableCollection<KeyValuePair<uint, string>>? Channels { get; set; }
+
+        /// <summary>
+        /// Bindable dictionary of Users in the Server
+        /// </summary>
+        [JsonIgnore()]
+        public BindableCollection<KeyValuePair<uint, string>>? Users { get; set; }
+
+        /// <summary>
+        /// Bindable dictionary of Roles in the Server
+        /// </summary>
+        [JsonIgnore()]
+        public BindableCollection<KeyValuePair<uint, string>>? Roles { get; set; }
+
+        /// <summary>
+        /// Timezone used when setting off the tasks
+        /// </summary>
+        [JsonPropertyName("timezone")]
+        public string Timezone { get; set; } = "";
+
+        /// <summary>
+        /// Bindable list of tasks
+        /// </summary>
+        [JsonPropertyName("tasks")]
+        public BindableCollection<TaskViewModel>? Tasks { get; set; }
+
+        /// <summary>
+        /// Bindable dictionary of vars
+        /// </summary>
+        [JsonPropertyName("vars")]
+        public BindableCollection<KeyValuePair<string, string>>? Vars { get; set; }
+    }
+}
