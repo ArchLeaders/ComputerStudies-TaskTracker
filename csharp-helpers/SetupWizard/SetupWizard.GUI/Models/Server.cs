7faf8d680e97685be70c02850cf73510b89ede4c
ArchLeaders <archparliament@gmail.com>
2022-05-11 17:43:20 -0700

    Use generic types; GetShellBase function

--- csharp-helpers/SetupWizard/SetupWizard.GUI/Models/Server.cs
+++ csharp-helpers/SetupWizard/SetupWizard.GUI/Models/Server.cs
@@ -1,5 +1,5 @@
 ﻿using SetupWizard.GUI.ViewModels;
-using Stylet;
+using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
@@ -7,24 +7,6 @@ namespace SetupWizard.GUI.Models
 {
     public class Server
     {
-        /// <summary>
-        /// Bindable dictionary of Channels
-        /// </summary>
-        [JsonIgnore()]
-        public BindableCollection<KeyValuePair<uint, string>>? Channels { get; set; }
-
-        /// <summary>
-        /// Bindable dictionary of Users in the Server
-        /// </summary>
-        [JsonIgnore()]
-        public BindableCollection<KeyValuePair<uint, string>>? Users { get; set; }
-
-        /// <summary>
-        /// Bindable dictionary of Roles in the Server
-        /// </summary>
-        [JsonIgnore()]
-        public BindableCollection<KeyValuePair<uint, string>>? Roles { get; set; }
-
         /// <summary>
         /// Timezone used when setting off the tasks
         /// </summary>
@@ -35,12 +17,58 @@ namespace SetupWizard.GUI.Models
         /// Bindable list of tasks
         /// </summary>
         [JsonPropertyName("tasks")]
-        public BindableCollection<TaskViewModel>? Tasks { get; set; }
+        public List<TaskModel> Tasks { get; set; } = new();
 
         /// <summary>
         /// Bindable dictionary of vars
         /// </summary>
         [JsonPropertyName("vars")]
-        public BindableCollection<KeyValuePair<string, string>>? Vars { get; set; }
+        public Dictionary<string, string> Vars { get; set; } = new();
+
+        public ShellViewModel GetShellBase()
+        {
+            ShellViewModel shell = new(null);
+
+            for (int i = 0; i < Tasks.Count; i++)
+            {
+                TaskModel task = Tasks[i];
+                TaskViewModel viewTask = new(i);
+
+                viewTask.Time = task.Time;
+                viewTask.DateTime = DateTime.Parse(task.Time);
+                viewTask.Message = task.Message
+                    .Replace("\\\"", "\"")
+                    .Replace("\\r", "\r")
+                    .Replace("\\n", "\n");
+
+                viewTask.Mon = task.Days["Mon"];
+                viewTask.Tue = task.Days["Tue"];
+                viewTask.Wed = task.Days["Wed"];
+                viewTask.Thu = task.Days["Thu"];
+                viewTask.Fri = task.Days["Fri"];
+                viewTask.Sat = task.Days["Sat"];
+                viewTask.Sun = task.Days["Sun"];
+
+                foreach (var var in task.Sequence)
+                {
+                    viewTask.Sequence += $"{var.Key}(";
+
+                    for (int v = 0; v < var.Value.Count; v++)
+                    {
+                        if (v+1 == var.Value.Count)
+                            viewTask.Sequence += $"{var.Value[v]}); ";
+                        else
+                            viewTask.Sequence += $"{var.Value[v]}, ";
+                    }
+                }
+
+                shell.Tasks.Add(viewTask);
+            }
+
+            foreach (var var in Vars)
+                shell.Vars.Add(new(var.Key, var.Value));
+
+            return shell;
+        }
     }
 }
