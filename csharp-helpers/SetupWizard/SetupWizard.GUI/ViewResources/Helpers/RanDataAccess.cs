44f6dc92b245291e4c0a8605bef078962221ace3
ArchLeaders <archparliament@gmail.com>
2022-05-11 17:52:26 -0700

    Use real data in random access; remove unused namespaces

--- csharp-helpers/SetupWizard/SetupWizard.GUI/ViewResources/Helpers/RanDataAccess.cs
+++ csharp-helpers/SetupWizard/SetupWizard.GUI/ViewResources/Helpers/RanDataAccess.cs
@@ -12,30 +12,30 @@ namespace SetupWizard.GUI.ViewResources.Helpers
 
             tasks.Add(new(0)
             {
-                Time = TimeOnly.Parse("1:20PM"),
-                Channel = new(0001, "data-access"),
-                Role = new(0001, "tasks"),
-                User = new(0001, "ArchLeaders"),
+                Time = "1:20PM",
+                Channel = new(935399672988712973, "data-access"),
+                Role = new(935400511773999104, "Admin"),
+                User = new(728823685015797770, "ArchLeaders"),
                 Sequence = "user(user_a, user_m)",
                 Message = "@role $user needs to do somethig at $time_now"
             });
 
             tasks.Add(new(1)
             {
-                Time = TimeOnly.Parse("2:50PM"),
-                Channel = new(0001, "data-access"),
-                Role = new(0001, "tasks"),
-                User = new(0001, "ArchLeaders"),
+                Time = "2:50PM",
+                Channel = new(935399672988712973, "data-access"),
+                Role = new(935400511773999104, "Admin"),
+                User = new(728823685015797770, "ArchLeaders"),
                 Sequence = "user(user_m)",
                 Message = "@role $user needs to do somethig at $time_now"
             });
 
             tasks.Add(new(2)
             {
-                Time = TimeOnly.Parse("3:25PM"),
-                Channel = new(0001, "data-access"),
-                Role = new(0001, "tasks"),
-                User = new(0001, "ArchLeaders"),
+                Time = "3:25PM",
+                Channel = new(935399672988712973, "data-access"),
+                Role = new(935400511773999104, "Admin"),
+                User = new(728823685015797770, "ArchLeaders"),
                 Sequence = "user(user_m, user_a)",
                 Message = "@role $user needs to do somethig at $time_now"
             });
