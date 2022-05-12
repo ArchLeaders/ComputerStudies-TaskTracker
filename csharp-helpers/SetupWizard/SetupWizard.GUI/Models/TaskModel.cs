649867bf518fcfe0e13c44dab2e6f1fc7acf59c7
ArchLeaders <archparliament@gmail.com>
2022-05-11 17:44:47 -0700

    Generic task class (condenced)

---
+++ csharp-helpers/SetupWizard/SetupWizard.GUI/Models/TaskModel.cs
@@ -0,0 +1,68 @@
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+using SetupWizard.GUI.ViewModels;
+using Stylet;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+
+namespace SetupWizard.GUI.Models
+{
+    public class TaskModel
+    {
+        public Dictionary<string, bool> Days { get; set; } = new();
+        public Dictionary<string, List<string>> Sequence { get; set; } = new();
+
+        public string Time { get; set; }
+        public string Message { get; set; }
+        public ulong Channel { get; set; }
+        public ulong Role { get; set; }
+        public ulong User { get; set; }
+
+        [JsonIgnore()]
+        public static BindableCollection<KeyValuePair<ulong, string>> Channels { get; set; } = new();
+
+        [JsonIgnore()]
+        public static BindableCollection<KeyValuePair<ulong, string>> Roles { get; set; } = new();
+
+        [JsonIgnore()]
+        public static BindableCollection<KeyValuePair<ulong, string>> Users { get; set; } = new();
+
+        [JsonConstructor]
+        public TaskModel() { }
+
+        public TaskModel(TaskViewModel task)
+        {
+            Message = task.Message
+                .Replace("\"", "\\\"")
+                .Replace("\r", "\\r")
+                .Replace("\n", "\\n");
+
+            Time = task.Time;
+            Channel = task.Channel.Key;
+            Role = task.Role.Key;
+            User = task.User.Key;
+
+            Days.Add(nameof(task.Mon), task.Mon);
+            Days.Add(nameof(task.Tue), task.Tue);
+            Days.Add(nameof(task.Wed), task.Wed);
+            Days.Add(nameof(task.Thu), task.Thu);
+            Days.Add(nameof(task.Fri), task.Fri);
+            Days.Add(nameof(task.Sat), task.Sat);
+            Days.Add(nameof(task.Sun), task.Sun);
+
+            if (task.Sequence != null && task.Sequence != "")
+            {
+                string[] sqVars = task.Sequence.Replace(" ", "").Split(';');
+
+                foreach (var sqVar in sqVars)
+                {
+                    string[] sqFunc = sqVar.Replace(")", "").Split('(');
+                    string[] sqArgs = sqFunc[1].Split(',');
+
+                    Sequence.Add(sqFunc[0], sqArgs.ToList());
+                }
+            }
+        }
+    }
+}
