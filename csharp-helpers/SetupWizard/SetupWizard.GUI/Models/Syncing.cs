994bdce2f6ac4ad0971f0b87b22745980fea53bd
ArchLeaders <archparliament@gmail.com>
2022-05-11 17:43:58 -0700

    Wait for completetion; guild meta data syncing

--- csharp-helpers/SetupWizard/SetupWizard.GUI/Models/Syncing.cs
+++ csharp-helpers/SetupWizard/SetupWizard.GUI/Models/Syncing.cs
@@ -1,6 +1,7 @@
 ﻿using Acheron.Web;
 using Discord;
 using Discord.WebSocket;
+using SetupWizard.GUI.ViewModels;
 using SetupWizard.GUI.ViewResources.Data;
 using Stylet;
 using System;
@@ -18,14 +19,14 @@ namespace SetupWizard.GUI.Models
     public class Syncing
     {
         private DiscordSocketClient Client { get; set; } = new();
-
-        private CancellationTokenSource CancelToken = new();
+        private bool Working { get; set; } = true;
         private string TempFile { get; set; } = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Temp\\server.io";
 
         public async Task<Server> Fetch(ulong settingsChannelId)
         {
             Server server = new();
             await Connect(OnReady);
+            return server;
 
             async Task OnReady()
             {
@@ -38,13 +39,64 @@ namespace SetupWizard.GUI.Models
 
                 // Deserialize server settings
                 File.WriteAllBytes(TempFile, bytes);
-                server = JsonSerializer.Deserialize<Server>(bytes);
+                server = JsonSerializer.Deserialize<Server>(bytes) ?? new();
 
-                // Disbose client
-                await Client.DisposeAsync();
+                // Get channels, roles and users
+                SocketGuildChannel guildChannel = (SocketGuildChannel)Client.GetChannel(settingsChannelId);
+                IGuild guild = guildChannel.Guild;
+
+                foreach (ITextChannel serverChannel in await guild.GetTextChannelsAsync())
+                {
+                    string category = "";
+
+                    if (serverChannel.CategoryId != null)
+                        category = $" ({((ICategoryChannel)await Client.GetChannelAsync(serverChannel.CategoryId ?? 0)).Name})";
+
+                    if (category == " (task-tracker-metadata)")
+                        continue;
+
+                    TaskModel.Channels.Add(new(serverChannel.Id, $"{serverChannel.Name}{category}"));
+                }
+
+                foreach (IRole role in guild.Roles)
+                {
+
+                    if (role.Name == "TaskTracker")
+                        continue;
+
+                    TaskModel.Roles.Add(new(role.Id, role.Name));
+                }
+
+                // The bot does not have access to the users of a guild, so it will be temporarily commented out
+                // 
+                // foreach (IUser user in await guild.GetUsersAsync())
+                // {
+                // 
+                //     if (user.Username == "TaskTracker#3825")
+                //         continue;
+                // 
+                //     TaskModel.Users.Add(new(user.Id, user.Username));
+                // }
+
+                // Complete task
+                Working = false;
             }
+        }
 
-            return server;
+        public async Task Send(SettingsViewModel settings)
+        {
+            // Create server
+            Server server = new();
+
+            foreach (var task in settings.ShellViewModel.Tasks)
+                server.Tasks.Add(new(task));
+
+            foreach (var _var in settings.ShellViewModel.Vars)
+                server.Vars.Add(_var.Key, _var.Value);
+
+            server.Timezone = settings.Timezone;
+
+            await Send(settings.ServerID, server);
         }
 
         public async Task Send(ulong serverId, Server server)
@@ -64,18 +116,26 @@ namespace SetupWizard.GUI.Models
                 IMessageChannel channel = (IMessageChannel)Client.GetChannel(serverId);
                 await channel.SendFileAsync(TempFile);
 
-                // Disbose client
-                await Client.DisposeAsync();
+                // Complete task
+                Working = false;
             }
         }
 
         public async Task Connect(Func<Task> onReady)
         {
+            // Start working
+            Working = true;
+
             // Connect the client
             await Client.LoginAsync(TokenType.Bot, User.DiscordBot);
             await Client.StartAsync();
             Client.Ready += onReady;
             Client.Log += Log;
+
+            while (Working)
+                await Task.Delay(1000);
+
+            await Client.DisposeAsync();
         }
 
         private Task Log(LogMessage ex)
