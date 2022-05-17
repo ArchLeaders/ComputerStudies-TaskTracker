228b351b5b8278f6da1b389a6c342a1b57b41916
ArchLeaders <archparliament@gmail.com>
2022-05-16 22:15:11 -0700

    Black formating; fix cog loader

--- src/bot.py
+++ src/bot.py
@@ -8,6 +8,7 @@ from nextcord import Color
 from nextcord.ext import commands
 from pathlib import Path
 
+
 def is_local_host() -> bool:
     try:
         # import local file to register the discord token
@@ -19,7 +20,9 @@ def is_local_host() -> bool:
 
         return True
 
-    except: { }
+    except:
+        {}
+
 
 #
 # bot client and config constants
@@ -29,19 +32,20 @@ def is_local_host() -> bool:
 is_local_host()
 
 SRC: str = os.environ["SRC"]  # local and remote source directory
-BOT = commands.Bot(command_prefix=('-')) # m-dash added for mobile support
-BOT_NAME = 'Task Tracker'
+BOT = commands.Bot(command_prefix=("-"))  # m-dash added for mobile support
+BOT_NAME = "Task Tracker"
 
 # these will be client based when testing is complete
-DB_CHANNEL: int = 961123912505258024
-LOCAL_TIME = pytz.timezone('America/Vancouver')
+CHANNEL_DB: int = 961123912505258024
+CHANNEL_NEW_REG: int = 975930357545316393
+LOCAL_TIME = pytz.timezone("America/Vancouver")
 
 # prompt images
-NOTICE = 'https://cdn.discordapp.com/attachments/954237048275996693/954817191998546000/notice.png'
-INVALID = 'https://cdn.discordapp.com/attachments/954237048275996693/954817192678010911/invalid.png'
-WARNING = 'https://cdn.discordapp.com/attachments/954237048275996693/954817192199847936/warning.png'
-HELP = 'https://cdn.discordapp.com/attachments/954237048275996693/954817192426360952/help.png'
-ACESS_DENIED = 'https://cdn.discordapp.com/attachments/954237048275996693/954893350878724118/access_denied.png'
+NOTICE = "https://cdn.discordapp.com/attachments/954237048275996693/954817191998546000/notice.png"
+INVALID = "https://cdn.discordapp.com/attachments/954237048275996693/954817192678010911/invalid.png"
+WARNING = "https://cdn.discordapp.com/attachments/954237048275996693/954817192199847936/warning.png"
+HELP = "https://cdn.discordapp.com/attachments/954237048275996693/954817192426360952/help.png"
+ACESS_DENIED = "https://cdn.discordapp.com/attachments/954237048275996693/954893350878724118/access_denied.png"
 
 # prompt colors
 NOTICE_COLOR = Color(0x33DD85)
@@ -54,50 +58,63 @@ HELP_COLOR = Color(0x57A3ED)
 # helper functions
 #
 
+
 def update_database(database: str, obj, ext: str = None):
-    _ext = 'json' if ext else ext
-    Path(f'{SRC}/database/{database}.{ext}').write_text(json.dumps(obj, indent=4))
+    _ext = "json" if ext else ext
+    Path(f"{SRC}/database/{database}.{ext}").write_text(json.dumps(obj, indent=4))
 
 
 def load_database(database: str, ext: str = None):
-    _ext = ext if ext else 'json'
-    return json.loads(Path(f'{SRC}/database/{database}.{_ext}').read_text())
+    _ext = ext if ext else "json"
+    return json.loads(Path(f"{SRC}/database/{database}.{_ext}").read_text())
 
 
 def role(ctx: commands.Context, id: int) -> nextcord.Role:
     return ctx.channel.guild.get_role(id)
 
+
 #
 # on ready events
 #
 
+
 def load_cogs():
-    for file in Path(f'{SRC}/src/cogs').glob('**/*.py'):
+    for file in Path(f"{SRC}/src/cogs").glob("**/*.py"):
         """Load cogs in the cog directory"""
 
-        if file.is_file() and '_view.py' not in file.name:
+        if file.is_file() and "_view.py" not in file.name:
             # format name
             if is_local_host():
-                name = file.as_posix().replace('.py', '').replace(
-                    '/', '.').replace('src.cogs.', '')
+                name = (
+                    file.as_posix()
+                    .replace(".py", "")
+                    .replace("/", ".")
+                    .replace("src.cogs.", "")
+                )
             else:
-                name = file.as_posix().replace('.py', '').replace(
-                    '/app/src/cogs/', '').replace('/', '.')
+                name = (
+                    file.as_posix()
+                    .replace(".py", "")
+                    .replace("/app/src/cogs/", "")
+                    .replace("/", ".")
+                )
 
             # load cog
-            BOT.load_extension(f'cogs.{name}')
+            BOT.load_extension(f"cogs.{name}")
 
             # log success
-            print(f'Loaded: cogs.{name}')
+            print(f"Loaded: cogs.{name}")
 
 
 @BOT.event
 async def on_ready():
     # set status
-    await BOT.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.listening, name='tasks'))
+    await BOT.change_presence(
+        activity=nextcord.Activity(type=nextcord.ActivityType.listening, name="tasks")
+    )
 
     # log online status to the console
-    print(f'{BOT.user} has started.')
+    print(f"{BOT.user} has started.")
 
     load_cogs()
 
@@ -106,5 +123,6 @@ async def on_ready():
     #
     # SERVER_ID = ctx.guild()
 
+
 # run bot client
-BOT.run(os.environ['TOKEN'])
+BOT.run(os.environ["TOKEN"])
