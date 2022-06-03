a53a8e0e830bc033ad59a27cdc3ac700ca08c804
ArchLeaders <archparliament@gmail.com>
2022-06-03 01:09:56 -0700

    Rename events cog; increment sequence vars

--- src/cogs/tasks.py
+++ src/cogs/tasks.py
@@ -1,22 +1,15 @@
 import asyncio
-import pytz
 import json
 
 from bot import BOT, CHANNEL_DB, CHANNEL_NEW_REG, HELP, HELP_COLOR, SRC
-from datetime import datetime
-from nextcord import Embed, File, TextChannel, PermissionOverwrite, CategoryChannel
+from nextcord import File, Embed
 from nextcord.ext import commands, tasks
-from oead import yaz0
 from pathlib import Path
-
-SERVER_CONFIG = None
-SERVER_CONFIG_ID: int = 0
+from utils import get_settings_channel, get_settings_data, get_time, set_settings_data
 
 
-class Events(commands.Cog):
+class Tasks(commands.Cog):
     """Handles events"""
-
-    sequence_pos: dict = {}
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
@@ -45,37 +38,31 @@ class Events(commands.Cog):
 
         # iterate the servers and handle each server accordingly
         for server, _ in servers.items():
-            print(
-                f"Execute '{server}' at {self.get_time('America/Vancouver', '%I:%M %p')}"
-            )
-            py_tasks.append(asyncio.create_task(self.handle_server(server)))
+            print(f"Execute '{server}' at {get_time('America/Vancouver', '%I:%M %p')}")
+            py_tasks.append(asyncio.create_task(self.handle_server(int(server))))
 
         # await all tasks
         asyncio.gather(*py_tasks)
 
     async def handle_server(self, id: int):
 
-        # get local settings
-        server_channel = await BOT.get_channel(int(id)).history(limit=1).flatten()
-        settings = await server_channel[0].attachments[0].read()
+        update_setting: bool = False
 
-        # decompress if compressed
-        if settings[0 - 4] == b"Yaz0":
-            settings = json.loads(yaz0.decompress(settings))
-        else:
-            settings = json.loads(settings)
+        # get local settings
+        settings = await get_settings_data(BOT.get_channel(id).guild.id)
 
         for task in settings["tasks"]:
 
             # skip tasks that are not set for the current time
-            if task["Time"] != self.get_time(settings["timezone"], "%I:%M %p"):
+            if task["Time"] != get_time(settings["timezone"], "%I:%M %p"):
                 continue
 
             # check the day of the week
-            if task["Days"][self.get_time(settings["timezone"], "%a")] == False:
+            if task["Days"][get_time(settings["timezone"], "%a")] == False:
                 continue
 
             # get the task channel and message
+            update_setting = True
             channel = BOT.get_channel(task["Channel"])
 
             # set message and replace basic vars
@@ -97,7 +84,16 @@ class Events(commands.Cog):
             for key, value in task["Sequence"].items():
 
                 # set sequence values
-                message = message.replace(f"${key}", value[0])
+                message = message.replace(
+                    f"${key}", task["Sequence"][key][task["Session"][key]]
+                )
+
+                # advance session
+                task["Session"][key] = (
+                    task["Session"][key] + 1
+                    if len(task["Sequence"][key]) > (task["Session"][key] + 1)
+                    else 0
+                )
 
             # iterate vars replacing every instance found in the message
             for key, value in settings["vars"].items():
@@ -108,73 +104,31 @@ class Events(commands.Cog):
             # send the message in channel
             await channel.send(message)
 
-    def get_hour(self, tz: str) -> int:
-        return int(datetime.now(pytz.timezone(tz)).strftime("%I"))
-
-    def get_time(self, tz: str, strftime) -> str:
-        return datetime.now(pytz.timezone(tz)).strftime(
-            strftime.replace("%I", str(self.get_hour(tz)))
-        )
+        # update the server settings
+        if update_setting == True:
+            set_settings_data(id, settings)
 
     @commands.command()
     async def register(self, ctx: commands.Context):
         """Register the server"""
 
-        # send a loading message
         reg = await ctx.send("Registering TaskTracker...")
 
-        # get the channel in context
-        guild = ctx.guild
-
-        # add settings config
-        create: bool = True
-
-        # check for an existing task-tracker-metadata category
-        for guild_category in guild.channels:
-            if guild_category.name == "task-tracker-metadata":
-                create = False
-                category = guild_category
-
-        # create the task-tracker-metadata category
-        if create == True:
-            category = await guild.create_category(
-                "task-tracker-metadata",
-                overwrites={
-                    guild.default_role: PermissionOverwrite(**{"view_channel": False})
-                },
-            )
-
-        # reset create
-        create = True
-
-        # check for an existing server channel in the task-tracker-metadata category
-        for channel in category.channels:
-            if channel.name == "server":
-                create = False
-                settings = channel
-
-        # create a server channel in the task-tracker-metadata category
-        if create == True:
-            settings: TextChannel = await guild.create_text_channel(
-                "server", category=category
-            )
-
-        # create the default config file to the settings channel
-        defaults = {"timezone": "", "vars": {}, "tasks": []}
-
         # upload the default config file to the settings channel
+        settings = await get_settings_channel(ctx.guild.id)
+
         file = Path(f"{SRC}\\tmp.io")
-        file.write_text(json.dumps(defaults, indent=4))
-        await settings.send(file=File(fp=file, filename=f"server.io"))
+        file.write_text(json.dumps({"timezone": "", "vars": {}, "tasks": []}, indent=4))
+        await settings.send(file=File(fp=file, filename=f"server.io.json"))
         file.unlink()
 
         # update server list
-        server_list = BOT.get_channel(CHANNEL_DB).last_message
-
-        if not server_list:
-            server_list = {}
-        else:
-            server_list = await json.loads(server_list.attachments[0].read())
+        server_list = BOT.get_channel(CHANNEL_DB).history(limit=1).flatten()
+        server_list = (
+            {}
+            if not server_list[0]
+            else await json.loads(server_list[0].attachments[0].read())
+        )
 
         server_list[settings.id] = 0
 
@@ -200,7 +154,7 @@ class Events(commands.Cog):
         )
         embed.add_field(
             name="Step One:",
-            value=f"Download the [SetupWizard](https://github.com/ArchLeaders/ComputerStudies-TaskTracker/releases/latest) from GitHub and this [token file]({token_msg.attachments[0].url})"
+            value=f"Download the [SetupWizard.exe](https://github.com/ArchLeaders/ComputerStudies-TaskTracker/releases/latest) from GitHub and this [token file]({token_msg.attachments[0].url})"
             + " to configure TaskTracker with your server.",
             inline=False,
         )
@@ -216,11 +170,11 @@ class Events(commands.Cog):
         try:
             await reg.delete()
         except:
-            {}
+            pass
 
         # send the embed with a comfirmation message
         await ctx.send(embed=embed)
 
 
 def setup(bot: commands.Bot):
-    bot.add_cog(Events(bot))
+    bot.add_cog(Tasks(bot))
