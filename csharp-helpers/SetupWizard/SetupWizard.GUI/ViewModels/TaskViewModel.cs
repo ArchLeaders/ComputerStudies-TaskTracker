7c43de99ce9f71e14189b2bb073d8d848657adf6
ArchLeaders <archparliament@gmail.com>
2022-06-09 11:09:23 -0700

    Format TaskViewModel

--- csharp-helpers/SetupWizard/SetupWizard.GUI/ViewModels/TaskViewModel.cs
+++ csharp-helpers/SetupWizard/SetupWizard.GUI/ViewModels/TaskViewModel.cs
@@ -16,104 +16,89 @@ namespace SetupWizard.GUI.ViewModels
 
         #region Bindings
 
-        private string _time = "12:00PM";
-        public string Time
-        {
+        private string _time = "12:00 PM";
+        public string Time {
             get => _time;
-            set
-            {
+            set {
                 DateTime = DateTime.Parse(value.ToString());
                 SetAndNotify(ref _time, value);
             }
         }
 
-        private DateTime _dateTime = DateTime.Parse("12:00PM");
-        public DateTime DateTime
-        {
+        private DateTime _dateTime = DateTime.Parse("12:00 PM");
+        public DateTime DateTime {
             get => _dateTime;
             set => SetAndNotify(ref _dateTime, value);
         }
 
         private bool _mon = true;
-        public bool Mon
-        {
+        public bool Mon {
             get => _mon;
             set => SetAndNotify(ref _mon, value);
         }
 
         private bool _tue = true;
-        public bool Tue
-        {
+        public bool Tue {
             get => _tue;
             set => SetAndNotify(ref _tue, value);
         }
 
         private bool _wed = true;
-        public bool Wed
-        {
+        public bool Wed {
             get => _wed;
             set => SetAndNotify(ref _wed, value);
         }
 
         private bool _thu = true;
-        public bool Thu
-        {
+        public bool Thu {
             get => _thu;
             set => SetAndNotify(ref _thu, value);
         }
 
         private bool _fri = true;
-        public bool Fri
-        {
+        public bool Fri {
             get => _fri;
             set => SetAndNotify(ref _fri, value);
         }
 
         private bool _sat = false;
-        public bool Sat
-        {
+        public bool Sat {
             get => _sat;
             set => SetAndNotify(ref _sat, value);
         }
 
         private bool _sun = false;
-        public bool Sun
-        {
+        public bool Sun {
             get => _sun;
             set => SetAndNotify(ref _sun, value);
         }
 
         private KeyValuePair<ulong, string> _channel;
-        public KeyValuePair<ulong, string> Channel
-        {
+        public KeyValuePair<ulong, string> Channel {
             get => _channel;
             set => SetAndNotify(ref _channel, value);
         }
 
         private KeyValuePair<ulong, string> _role;
-        public KeyValuePair<ulong, string> Role
-        {
+        public KeyValuePair<ulong, string> Role {
             get => _role;
             set => SetAndNotify(ref _role, value);
         }
 
         private KeyValuePair<ulong, string> _user;
-        public KeyValuePair<ulong, string> User
-        {
+        public KeyValuePair<ulong, string> User {
             get => _user;
             set => SetAndNotify(ref _user, value);
         }
 
         private string? _sequence;
-        public string? Sequence
-        {
+        public string? Sequence {
             get => _sequence;
             set => SetAndNotify(ref _sequence, value);
         }
 
         private string _message = "";
-        public string Message
-        {
+        public string Message {
             get => _message;
             set => SetAndNotify(ref _message, value);
         }
@@ -123,22 +108,19 @@ namespace SetupWizard.GUI.ViewModels
         #region Server Data
 
         private BindableCollection<KeyValuePair<ulong, string>> _channels = TaskModel.Channels;
-        public BindableCollection<KeyValuePair<ulong, string>> Channels
-        {
+        public BindableCollection<KeyValuePair<ulong, string>> Channels {
             get => _channels;
             set => SetAndNotify(ref _channels, value);
         }
 
         private BindableCollection<KeyValuePair<ulong, string>> _roles = TaskModel.Roles;
-        public BindableCollection<KeyValuePair<ulong, string>> Roles
-        {
+        public BindableCollection<KeyValuePair<ulong, string>> Roles {
             get => _roles;
             set => SetAndNotify(ref _roles, value);
         }
 
         private BindableCollection<KeyValuePair<ulong, string>> _users = TaskModel.Users;
-        public BindableCollection<KeyValuePair<ulong, string>> Users
-        {
+        public BindableCollection<KeyValuePair<ulong, string>> Users {
             get => _users;
             set => SetAndNotify(ref _users, value);
         }
